9f28f4d1e4520b8bc867d697a3448a411ce71c44
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-26 00:29:48 -0300

    Here we configure the types of security, including wich url to access each end-point, each roles each user has to have to access a specific url, and disable csrf

--- src/main/java/com/github/pedroluiznogueira/microservices/couponservice/security/config/WebSecurityConfig.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/couponservice/security/config/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package com.github.pedroluiznogueira.microservices.couponservice.security.config
 import com.github.pedroluiznogueira.microservices.couponservice.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -12,9 +14,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+    // manager component
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // provider component
         auth.userDetailsService(userDetailsService);
+    }
+
+    // configure how and wich urls access api
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http
+            .authorizeRequests()
+            .mvcMatchers(HttpMethod.GET, "/coupons/find/{code}")
+            .hasAnyRole("USER", "ADMIN")
+            .mvcMatchers(HttpMethod.POST, "/coupons/create")
+            .hasRole("ADMIN")
+            .and()
+            .csrf()
+            .disable();
     }
 
     @Bean
