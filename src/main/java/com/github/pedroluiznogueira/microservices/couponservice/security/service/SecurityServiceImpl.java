05a0c20157c024fc0e72a354be7cd5e62520fe83
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-27 16:58:33 -0300

    Creating a custom Login method, that will be used by the manager component

--- src/main/java/com/github/pedroluiznogueira/microservices/couponservice/security/service/SecurityServiceImpl.java
+++ src/main/java/com/github/pedroluiznogueira/microservices/couponservice/security/service/SecurityServiceImpl.java
@@ -1,9 +1,35 @@
 package com.github.pedroluiznogueira.microservices.couponservice.security.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SecurityServiceImpl implements SecurityService {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Override
     public boolean login(String userName, String password) {
-        return false;
+        // we need a UserDetails object to create a token
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        // manager component verifies user's token
+        authenticationManager.authenticate(token);
+
+        // token is stored in security context if authenticated
+        boolean result = token.isAuthenticated();
+        if (result) SecurityContextHolder.getContext().setAuthentication(token);
+
+        return result;
     }
 }
